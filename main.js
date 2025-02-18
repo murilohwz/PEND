fb9248babd5c201a14e7f0975a965cdf16a7ed20
murilohwz <159180416+murilohwz@users.noreply.github.com>
2025-02-18 15:29:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+et contato1_nome = "Maxwell Wright";
+let contato1_telefone = "0191 719 6495";
+let contato1_email = "Curabitur.egestas.nunc@nonummyac.co.uk";
+
+let contato2_nome = "Raja Villareal";
+let contato2_telefone = "0866 398 2895";
+let contato2_email = "posuere.vulputate";
+
+let contato3_nome = "Helen Richards";
+let contato3_telefone = "0800 1111";
+let contato3_email = "libero@convallis.edu";
+
+
+console.log(contato1_nome, contato1_telefone, contato1_email);
+console.log(contato3_nome, contato3_telefone, contato3_email);
