03fb8a858e3750d7c3c3b8d98826f609449c1b74
murilohwz <159180416+murilohwz@users.noreply.github.com>
2025-02-18 15:55:19 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+//1
+let nome = "Murilo";
+let cidade = "votorantim";
+let idade = 17
+console.log(nome);
+console.log(cidade);
+console.log(idade);
+
+//2
+var cidade_natal
+
+//3
+var altura;
+console.log(altura); // não é exibida"undefined"
+
+//4 É exibida
+let altura =11;
+console.log(altura);
+
+//5 Uncaught ReferenceError: MuriloIdade is not defined
+"use strict";
+
+MuriloIdade = 10;
+console.log(MuriloIdade);
+
+//6 
+let contador = 0
+contador +=1;
+contador +=2;
+contador +=3;
+console.log(contador)
+
+//7 o mesmo valor do primeiro é exibido
+let pi = 3.14;
+console.log(pi)
+pi = 3.16
+
+//8 
+
